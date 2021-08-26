22dc3281124214a5860b441e97fbc643b6a095a4
afsana18 <77396997+afsana18@users.noreply.github.com>
2021-08-26 23:16:09 +0600

    Add files via upload

---
+++ test.java
@@ -0,0 +1,27 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test {
+
+	private static final By By = null;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        //got to the link
+        driver.get("http://automationpractice.com/index.php");
+       
+	WebElement element= driver.findElement(By.className("login"));
+	element.click();
+	element=driver.findElement(By.id("email"));
+	element.sendKeys("afsana.afrin96@gmail.com");
+	driver.findElement(By.id("passwd"))
+	
+	}
+
+}
